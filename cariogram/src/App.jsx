4907b4bafbc005f2dd50b18c7a2fd69f920cbd18
hdfredl <dat.leef@gmail.com>
2025-01-14 08:32:17 +0100

    he

--- cariogram/src/App.jsx
+++ cariogram/src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/home";
 import LandingPage from "./pages/landingpage";
-import React, { useEffect } from "react";
+import React from "react";
 
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/cariogram">
         <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
           <Route path="/landingpage" element={<LandingPage />} />
         </Routes>
